f73a2011ebbe2e491657cb009bca018ca742a484
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-21 16:35:52 +0100

    // Keydown event

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const response = document.querySelector("p");
 
 btn1.addEventListener("click", () => {
   // Code à exécuter lorsque btn1 est cliqué
-  Console.log( "click sur le bouton") ;
+  // Console.log( "click sur le bouton") ;
   });
 
   questionContainer.addEventListener("click", () => {
@@ -32,7 +32,7 @@ const mousemove = document.querySelector('.mousemove');
 window.addEventListener('mousemove',
   (e) => {
 
-    console.log(e.pageX);
+    // console.log(e.pageX);
     
    mousemove.style.left = e.pageX + "px";
   mousemove.style.top = e.pageY + "px";
@@ -66,4 +66,39 @@ questionContainer.addEventListener(
 response.addEventListener('mouseover',
   () => {
   response.style.transform = "rotate(1deg)";
+  });
+
+  // Dépréciation : L'événement keypress est déprécié dans les versions récentes de JavaScript. Il est recommandé d'utiliser keydown ou keyup à la place.
+
+// Propriétés de l'événement :
+
+// event.key : Renvoie la valeur de la touche pressée.
+// event.code : Renvoie le code physique de la touche.
+// Alternatives
+// keydown : Détecte quand une touche est enfoncée.
+// keyup : Détecte quand une touche est relâchée.
+// Exemple avec keydown
+
+document.addEventListener('keydown', function(event) {
+    // console.log('Touche enfoncée :', event.key);
+});
+
+// Keydown event
+
+const KeydownContainer = document.querySelector(".Keydown");
+// console.log(KeydownContainer);
+
+window.addEventListener("Keydown",
+  (e) => {
+  console.log(e);
+  // console.log(e.key)
+
+  // key.textContent = e.key;
+  // if (e.key === "j") {
+  // KeydownContainer.style.background = "pink";
+  // } else if (e.key === "h") {
+  // KeydownContainer.style.background = "teal";
+  // } else {
+  // KeydownContainer.style.background = "red";
+  // }
   });
