5e474a56f4def8fce1248a4a73dc9536fd92fcd3
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-21 16:01:33 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.querySelector("#btn-2");
+const response = document.querySelector("p");
+
+
+btn1.addEventListener("click", () => {
+  // Code à exécuter lorsque btn1 est cliqué
+  Console.log( "click sur le bouton") ;
+  });
+
+  questionContainer.addEventListener("click", () => {
+    questionContainer.classList.toggle('question-click')
+    });
+
+    btn1.addEventListener("click", () => {
+      btn1.style.background = 'green';
+      response.style.background = 'green';
+      response.classList.add('show-response');
+      });
+
+      btn2.addEventListener("click", () => {
+        btn2.style.background = 'red';
+        response.style.background = 'red';
+        response.classList.add('show-response');
+        });
+
+
+   
+
+ 
+
+
